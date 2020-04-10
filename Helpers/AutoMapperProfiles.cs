09a8001b29ac26f11dcdc90d044f9465da2d51a3
ABDEL <ABDEL@outlook.fr>
2020-04-10 16:45:48 +0200

    implementation of UpdateUser methode

--- Helpers/AutoMapperProfiles.cs
+++ Helpers/AutoMapperProfiles.cs
@@ -22,6 +22,7 @@ namespace DattingApp.API.Helpers
                  opt.MapFrom(src => src.DateOfBirth.CalculateAge()));
 
             CreateMap<Photo, PhotosForDetailedDto>();
+            CreateMap<UserForUpdateDto, User>();
         }
         
     }
