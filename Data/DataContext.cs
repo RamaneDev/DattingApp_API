5cc8cc9f5029a3a323a464ab2a57930ebdf3ae25
ABDEL <ABDEL@outlook.fr>
2020-04-12 02:52:47 +0200

    remove Dbset<Value> from DataContext

--- Data/DataContext.cs
+++ Data/DataContext.cs
@@ -7,8 +7,6 @@ namespace DattingApp.API.Data
     {
         public DataContext(DbContextOptions<DataContext> options) : base(options) { }
 
-        public DbSet<Value> Values { get; set; }
-
         public DbSet<User> Users {get; set ;}
 
         public DbSet<Photo> Photos { get; set; }
