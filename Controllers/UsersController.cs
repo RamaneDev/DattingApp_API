c4d1924f6dfe58ccca7ab342462f9d0dfec069d1
ABDEL <ABDEL@outlook.fr>
2020-04-19 20:24:24 +0200

    Action Filter to update the LastActive User Date

--- Controllers/UsersController.cs
+++ Controllers/UsersController.cs
@@ -5,11 +5,12 @@ using System.Threading.Tasks;
 using AutoMapper;
 using DattingApp.API.Data;
 using DattingApp.API.Dtos;
+using DattingApp.API.Helpers;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace DattingApp.API.Controllers
-{
+{   [ServiceFilter(typeof(LogUserActivity))]
     [Authorize]
     [ApiController]
     [Route("[controller]")]
--- Startup.cs
+++ Startup.cs
@@ -56,6 +56,7 @@ namespace DattingApp.API
                         };
             
             });
+            services.AddScoped<LogUserActivity>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
