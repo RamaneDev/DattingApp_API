5be1367b2cfc8795142e50c3db54ee071292be77
ABDEL <ABDEL@outlook.fr>
2020-04-21 21:31:22 +0200

    Add pagination's class Helper

---
+++ Helpers/PaginationHeader.cs
@@ -0,0 +1,18 @@
+namespace DattingApp.API.Helpers
+{
+    public class PaginationHeader
+    {
+        public int ItemsPerPage { get; set; }
+        public int TotalItems { get; set; }
+        public int TotalPages { get; set; }
+        public int CurrentPage { get; set; }
+
+        public PaginationHeader(int currentPage, int itemsPerPage, int totalItems, int totalPages)
+        {
+            this.CurrentPage = currentPage;
+            this.TotalItems = totalItems;
+            this.ItemsPerPage = itemsPerPage;
+            this.TotalPages = totalPages;
+        }
+    }
+}
