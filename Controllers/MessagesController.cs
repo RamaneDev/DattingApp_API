0c511515d36fde943610b1b678b80c47d470846f
ABDEL <ABDEL@outlook.fr>
2020-04-29 02:37:12 +0200

    mark as read

--- Controllers/MessagesController.cs
+++ Controllers/MessagesController.cs
@@ -125,5 +125,26 @@ namespace DattingApp.API.Controllers
 
         }
 
+        [HttpPost("{id}/read")]
+        public async Task<IActionResult> MarkAsRead(int userId, int id)
+        {
+            
+             if (userId != int.Parse(User.FindFirst(ClaimTypes.NameIdentifier).Value))
+                return Unauthorized();
+            
+             var messageFromRepo = await _repo.GetMessage(id);
+
+            if (messageFromRepo.RecipientId != userId)
+                return Unauthorized();
+            
+            messageFromRepo.IsRead = true;
+            messageFromRepo.DateRead = DateTime.Now;
+
+            await _repo.SaveAll();
+
+            return NoContent();
+        
+        }
+
     }
 }
